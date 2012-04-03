34989c25c06f99d787d3dffab24976e2a4f370d5
Paul M Edwards <pauledwards@gmail.com>
2012-04-03 00:48:11 -0700

    Simple GUI generated to wire-up

---
+++ src/com/paul/thepcphysician/android/DroidSum/DroidSum.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.paul.thepcphysician.android.DroidSum;
+
+import android.app.Application;
+
+/**
+ * @author Paul M Edwards
+ *
+ */
+public class DroidSum extends Application {
+
+	/**
+	 * 
+	 */
+	public DroidSum() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
